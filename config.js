13079503568b046059ec5a302eab2dc500b02df2
sweetntrades <nick.44sweet@gmail.com>
2026-09-20 16:18:43 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// After deploying the Vercel backend, replace the URL below.
+// Example: https://your-car-dex-api.vercel.app/api/identify
+window.CARDEX_CONFIG = {
+  API_URL: "PASTE_YOUR_VERCEL_URL_HERE/api/identify"
+};
