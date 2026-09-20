91a711b0db3333510f6e6709b6cd21b3361cb358
sweetntrades <nick.44sweet@gmail.com>
2026-09-20 17:51:21 -0600

    Connect CAR-DEX to AI backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // After deploying the Vercel backend, replace the URL below.
 // Example: https://your-car-dex-api.vercel.app/api/identify
 window.CARDEX_CONFIG = {
-  API_URL: "PASTE_YOUR_VERCEL_URL_HERE/api/identify"
+  API_URL: "https://script.google.com/macros/s/AKfycbz8HqhqohzArNo5y6sKGQFkz6D--SgK5w_UHulCERklyNKhCVPEsrK21omvBnQ861a2/exec/api/identify"
 };
